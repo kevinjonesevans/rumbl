ac0a35c6cd5076a2a6c264cebc38ea4a58978ace
Kevin Jones-Evans <jonesevans.kevin.t@gmail.com>
2019-02-13 23:17:27 -0500

    Fix cowboy versioning issue

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Rumbl.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:comeonin, "~> 2.0"},
-     {:plug, "~> 1.3"}]
+     {:plug, "~> 1.3"},
+     {:plug_cowboy, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
