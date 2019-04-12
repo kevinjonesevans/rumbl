d39bca41390416ed0452a7d46b0c0d3ff97314fd
Kevin Jones-Evans <jonesevans.kevin.t@gmail.com>
2019-04-12 14:02:17 -0400

    Adds an annotation view, schedules their appearance and makes them clickable

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -28,8 +28,19 @@ let Video = {
       this.renderAnnotation(msgContainer, resp)
     })
 
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+                    e.target.parentNode.getAttribute("data-seek")
+      if (!seconds) { return }
+
+      Player.seekTo(seconds)
+    })
+
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("ok", resp => {
+        this.scheduleMessages(msgContainer, resp.annotations)
+      })
       .receive("error", reason => console.log("join failed", reason))
   },
 
@@ -44,11 +55,37 @@ let Video = {
 
     template.innerHTML = `
     <a href="#" data-seek="${this.esc(at)}">
+      [${this.formatTime(at)}]
       <b>${this.esc(user.username)}</b>: ${this.esc(body)}
     </a>
     `
     msgContainer.appendChild(template)
     msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations) {
+    setTimeout(() => {
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer) {
+    return annotations.filter( ann => {
+      if (ann.at > seconds) {
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
+  },
+
+  formatTime(at) {
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14,5)
   }
 }
 
